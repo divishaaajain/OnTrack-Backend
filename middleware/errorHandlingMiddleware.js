82cb46c5080693181db39fe785ea2d5495bba004
Divisha <divishajain07@gmail.com>
2024-07-10 03:03:31 +0530

    Error handling

---
+++ middleware/errorHandlingMiddleware.js
@@ -0,0 +1,18 @@
+const CustomError = require("../utils/customErrorUtil");
+
+const errorHandler = (err, req, res, next) => {
+    console.error('Error:', err.message);
+
+    let statusCode = 500; // Default status code
+
+    // Check if error is an instance of CustomError
+    if (err instanceof CustomError) {
+        statusCode = err.statusCode;
+    }
+
+    res.status(statusCode).json({
+        error: err.message || 'Internal Server Error'
+    });
+};
+
+module.exports = errorHandler;
