66a02eb2800f79b4ea111dea73980bbd0c0bfea1
Divisha <divishajain07@gmail.com>
2024-07-11 02:33:42 +0530

    orders router

---
+++ routes/orderRouter.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { addOrder, getOrders, getOrder, deleteOrder } = require("../controllers/orderController");
+const auth = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/order", auth, async (req, res, next) => {
+    try {
+        const result = await addOrder({ 
+            orderDetails: req.body,  
+            user_id: req.user_id
+        });
+
+        res.status(201).send(result);
+    } catch (error) {
+        error.data = "ADD_ORDER"
+        next(error);
+    }
+});
+
+router.get("/orders", auth, async (req, res, next) => {
+    try {
+        const result = await getOrders({
+            user_id: req.user_id
+        });
+
+        res.status(200).send(result);
+    } catch (error) {
+        error.data = "GET_ORDERS"
+        next(error);
+    }
+});
+
+router.get("/order/:id", auth, async (req, res, next) => {
+    try {
+        const result = await getOrder({
+            user_id: req.user_id,
+            orderId: req.params.id
+        });
+
+        res.status(200).send(result);
+    } catch (error) {
+        error.data = "GET_ORDER"
+        next(error);
+    }
+});
+
+router.delete("/order/:id", auth, async (req, res, next) => {
+    try {
+        const result = await deleteOrder({
+            user_id: req.user_id,
+            orderId: req.params.id
+        });
+
+        res.status(200).send(result);
+    } catch (error) {
+        error.data = "DELETE_ORDER"
+        next(error);
+    }
+});
+
+
+module.exports = router;
