bd7df53df023214f44c393701f651e3f0e204149
Divisha <divishajain07@gmail.com>
2024-07-11 02:30:19 +0530

    items schema

--- models/itemsModel.js
+++ models/itemsModel.js
@@ -1,10 +1,12 @@
 class Item {
-    constructor({user_id, name, description, price, quantity, id}) {
+    constructor({ user_id, name, description, price, quantity, createdAt, updatedAt, id }) {
         this.user_id = user_id;
         this.name = name;
-        this.description = description; 
+        this.description = description;
         this.price = price;
         this.quantity = quantity;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
         this._id = id;
     }
 };
