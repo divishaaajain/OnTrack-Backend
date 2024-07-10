249dea7405ce7af0e55b59db235c6ff72d86cda5
Divisha <divishajain07@gmail.com>
2024-07-11 02:34:15 +0530

    order router added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const authRouter = require("./routes/authRouter");
 const itemsRouter = require("./routes/itemsRouter");
+const orderRouter = require("./routes/orderRouter");
 const errorHandler = require("./middleware/errorHandlingMiddleware");
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 // Routes
 app.use(authRouter);
 app.use(itemsRouter);
+app.use(orderRouter);
 app.use(errorHandler);
 
 module.exports = app;
