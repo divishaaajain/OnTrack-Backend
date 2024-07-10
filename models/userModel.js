17f44f2d0e43b174fd4fc4953d30337c2592c4af
Divisha <divishajain07@gmail.com>
2024-07-11 02:33:06 +0530

    user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,11 @@
 class User {
-    constructor({email, username, phone, password, id}) {
+    constructor({email, username, phone, password, createdAt, updatedAt, id }) {
         this.email = email;
         this.username = username; 
         this.phone = phone;
         this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
         this._id = id;
     }
 };
