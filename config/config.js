a4b9d402e056c3f117097ac849eeb1d846de66ff
Divisha <divishajain07@gmail.com>
2024-07-11 02:26:21 +0530

    environment variables configuration

--- config/config.js
+++ config/config.js
@@ -7,5 +7,9 @@ module.exports = {
     MONGODB_USER: process.env.MONGODB_USER,
     MONGODB_APP_NAME: process.env.MONGODB_APP_NAME,
     MONGODB_DBNAME: process.env.MONGODB_DBNAME,
-    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY
+    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_USERNMAE: process.env.MYSQL_USERNMAE,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_PORT: process.env.MYSQL_PORT
 };
